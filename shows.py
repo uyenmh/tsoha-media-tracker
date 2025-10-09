80fb9097390a52e4ea1bedfcfc15ddfcdddd5c1a
Uyen <152192561+uyenmh@users.noreply.github.com>
2025-10-10 01:07:57 +0300

    refactor shows.py

--- shows.py
+++ shows.py
@@ -3,25 +3,38 @@ from flask import abort, request, session
 from sqlalchemy import text
 
 def get_all_shows():
-    sql = text("SELECT id, title FROM shows ORDER BY title")
+    sql = text("""
+        SELECT id, title
+        FROM shows
+        ORDER BY title
+    """)
     return db.session.execute(sql).fetchall()
 
 def get_show_info(show_id):
-    sql = text("""SELECT s.title, s.type, s.description, s.release_date FROM shows s
-             WHERE s.id=:show_id""")
+    sql = text("""
+        SELECT s.title, s.type, s.description, s.release_date
+        FROM shows s
+        WHERE s.id=:show_id
+    """)
     result = db.session.execute(sql, {"show_id": show_id}).fetchone()
     return result
     
 def add_show(title, type, description, release_date):
-    sql = text("""INSERT INTO shows (title, type, description, release_date)
-             VALUES (:title, :type, :description, :release_date) RETURNING id""")
+    sql = text("""
+        INSERT INTO shows (title, type, description, release_date)
+        VALUES (:title, :type, :description, :release_date)
+        RETURNING id
+    """)
     show_id = db.session.execute(sql, {"title": title, "type": type, "description": description, "release_date": release_date}).fetchone()[0]
 
     db.session.commit()
     return show_id
     
 def remove_show(show_id):
-    sql = text("DELETE FROM shows WHERE id=:id")
+    sql = text("""
+        DELETE FROM shows
+        WHERE id=:id
+    """)
     db.session.execute(sql, {"id": show_id})
     db.session.commit()
  
@@ -49,12 +62,19 @@ def add_review(show_id, user_id, stars, comment):
     db.session.commit()
     
 def remove_own_review(show_id, user_id):
-    sql = text("DELETE FROM reviews WHERE show_id=:show_id AND user_id=:user_id")
+    sql = text("""
+        DELETE FROM reviews
+        WHERE show_id=:show_id AND user_id=:user_id
+    """)
     db.session.execute(sql, {"show_id": show_id, "user_id": user_id})
     db.session.commit()
     
 def has_user_reviewed(show_id, user_id):
-    sql = text("SELECT COUNT(*) FROM  reviews WHERE show_id=:show_id AND user_id=:user_id")
+    sql = text("""
+        SELECT COUNT(*)
+        FROM reviews
+        WHERE show_id=:show_id AND user_id=:user_id
+    """)
     result = db.session.execute(sql, {"show_id": show_id, "user_id": user_id}).fetchone()
     return result[0] > 0
     
